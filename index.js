9dac0e1e7d09cc1a94bfe885bf828ace86c7a3b0
rituraj005 <154808779+rituraj005@users.noreply.github.com>
2024-03-15 05:38:59 +0530

    Update index.js
    javascript has been added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // alert("hi!");
+document.querySelector(".category-button").addEventListener("click", (event) => {
+    // alert("button got clicked !");
+    // const clickedButton = document.querySelector("")
+    // console.log(this.getAttribute("id"));
+    // window.location.href = "http://127.0.0.1:3000/index.html"
+});
+
+const images = document.querySelectorAll("img");
+
+images.forEach(img => {
+  img.addEventListener("mouseenter", () => {
+    img.classList.add("blur");
+  });
+
+  img.addEventListener("mouseleave", () => {
+    img.classList.remove("blur");
+  });
+});
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+});
